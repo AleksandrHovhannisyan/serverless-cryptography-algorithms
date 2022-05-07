32273ebca9182884b92bce3a78bebf6df309cf52
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-07 08:22:17 -0400

    Add caesar cipher

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,58 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 
+const generateCaesarCipher = (shift, plaintextAlphabet) => {
+  if (shift === 0 || shift === plaintextAlphabet.length) {
+    throw new Error(
+      `Invalid caesar shift. Valid values range from ${1} through ${
+        plaintextAlphabet.length - 1
+      }.`
+    );
+  }
+
+  const cipherAlphabet = plaintextAlphabet.map((_symbol, index) => {
+    const newIndex = (index + shift) % plaintextAlphabet.length;
+    return plaintextAlphabet[newIndex].toLowerCase();
+  });
+
+  const encipher = (message) => {
+    const letters = message.split("");
+    return letters
+      .map((letter) => {
+        const index = plaintextAlphabet.indexOf(letter.toLowerCase());
+        return cipherAlphabet[index];
+      })
+      .join("");
+  };
+
+  const decipher = (message) => {
+    const letters = message.split("");
+    return letters
+      .map((letter) => {
+        const index = cipherAlphabet.indexOf(letter.toLowerCase());
+        return plaintextAlphabet[index];
+      })
+      .join("");
+  };
+
+  return { encipher, decipher };
+};
+
+const outputShortcode = (query) => {
+  switch (query.algorithm) {
+    case 'caesar': {
+      const caesarShift = generateCaesarCipher(Number(query.shift), query.alphabet.split(''));
+      const transform = caesarShift[query.operation];
+      return transform(query.message);
+    }
+    default: {
+      return;
+    }
+  }
+}
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addShortcode('output', outputShortcode);
+  
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "onrequest",
     functionsDir: "./netlify/functions/",
