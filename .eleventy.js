386bcac8e2fd3ad27a93e5c922f61e8116d8fb3d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-08 14:40:36 -0400

    Add paired shortcodes to consolidate duplicate layout logic

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 const { keylength, toSentenceCase, toISOString, cssminify, makeCipherFilter } = require("./11ty/filters");
+const { formShortcode, outputShortcode } = require("./11ty/shortcodes");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addFilter('cssminify', cssminify);
@@ -7,6 +8,8 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addFilter('keylength', keylength);
   eleventyConfig.addFilter('toSentenceCase', toSentenceCase);
   eleventyConfig.addFilter('makeCipher', makeCipherFilter);
+  eleventyConfig.addPairedShortcode('form', formShortcode);
+  eleventyConfig.addPairedShortcode('output', outputShortcode);
   
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "onrequest",
---
+++ 11ty/shortcodes.js
@@ -0,0 +1,21 @@
+function formShortcode(children) {
+  return `<section>
+    <h2 id="input">Input</h2>
+    <form action="${this.page.url}output/" method="get">
+        ${children}
+        <button type="submit">Submit</button>
+    </form>
+  </section>`;
+}
+
+function outputShortcode(children) {
+  return `<section>
+    <h2 id="output">Output</h2>
+    ${children}
+  </section>`
+}
+
+module.exports = {
+  formShortcode,
+  outputShortcode,
+};
