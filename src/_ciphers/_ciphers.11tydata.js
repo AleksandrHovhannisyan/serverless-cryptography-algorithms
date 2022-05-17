f597286e1de2b4b1b5dea26c796866e425ec5cf5
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-17 08:01:33 -0400

    Nest ciphers under /ciphers/ and compute permalinks programmatically

---
+++ src/_ciphers/_ciphers.11tydata.js
@@ -0,0 +1,13 @@
+module.exports = {
+  layout: "algorithm",
+  tags: ["algorithms", "ciphers"],
+  // eleventyComputed needed for data to be non-nullish
+  eleventyComputed: {
+    permalink: (data) => {
+      return {
+        build: `/ciphers/${data.page.fileSlug}/`,
+        [data.id]: `/ciphers/${data.page.fileSlug}/output/`,
+      };
+    },
+  }
+};
