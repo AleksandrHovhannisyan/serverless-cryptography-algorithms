4772e23c01da510b617f560266c9653bf2dbd719
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-25 06:56:45 -0400

    Fix broken test

--- 11ty/algorithms/atbashCipher/atbashCipher.test.js
+++ 11ty/algorithms/atbashCipher/atbashCipher.test.js
@@ -2,10 +2,10 @@ const generateAtbashCipher = require('./atbashCipher');
 const alphabets = require('../../../src/_data/alphabets');
 
 describe('atbash cipher', () => {
-  it(`enciphers "Hello, World!" to "svooldliow" and back to "helloworld"`, () => {
+  it(`enciphers "Hello, World!" to "SVOOLDLIOW" and back to "helloworld"`, () => {
     const cipher = generateAtbashCipher(alphabets.english);
     const ciphertext = cipher.encipher('Hello, World!');
-    expect(ciphertext).toStrictEqual('svooldliow');
+    expect(ciphertext).toStrictEqual('SVOOLDLIOW');
     expect(cipher.decipher(ciphertext)).toStrictEqual('helloworld');
   });
 });
